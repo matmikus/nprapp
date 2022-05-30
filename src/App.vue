ff617a6fd73943bfc02dd3315994ab1063bc951c
Mateusz Mikołajczak <mateusz.mikolajczak@grupawp.pl>
2022-05-30 20:33:16 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container">
+    <img alt="logo" class="logo" src="/logo.png" />
+  </div>
+</template>
+
+<style>
+    @import '@/assets/global.css';
+	
+	.container {
+		height: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	
+	.logo {
+        height: 50vh;
+	}
+</style>
